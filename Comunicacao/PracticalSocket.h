693299858160f4abed65563cefe26cd0f8225fc5
Diego Felix <diegobill@gmail.com>
2011-07-01 19:43:05 +0000

    esconder warnings do PracticalSocket.h
    git-svn-id: http://roboime.com.br/svn/RoboCup/Projeto Computacional/RobotIME/trunk@222 536dbed5-76cd-454d-90a9-4f62b6fdd354

--- Comunicacao/PracticalSocket.h
+++ Comunicacao/PracticalSocket.h
@@ -16,6 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#pragma warning(disable:4290)//ingnore this warnings, plz
 
 #ifndef __PRACTICALSOCKET_INCLUDED__
 #define __PRACTICALSOCKET_INCLUDED__
