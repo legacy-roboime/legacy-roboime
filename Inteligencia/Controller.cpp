193b5b0bfe1bafad6961ca52371b68c7fae9ba77
Diego Felix <diegobill@gmail.com>
2011-08-10 21:19:07 +0000

    git-svn-id: http://roboime.com.br/svn/RoboCup/Projeto Computacional/RobotIME/trunk@263 536dbed5-76cd-454d-90a9-4f62b6fdd354

--- Inteligencia/Controller.cpp
+++ Inteligencia/Controller.cpp
@@ -4,7 +4,12 @@
 namespace Inteligencia {
 	namespace Tactics {
 		Controller::Controller(int id, Robot* r, Stage* s, double speed) : Tactic(r,s){
+			_id = id;
+			_x = 0;
+			_y = 0;
 			_controller = new CXBOXController(id);
+			 //_kick = new Skills::Kick(r);
+			//_dribble = new Skills::Dribble(r);
 			_move = new Skills::Move(r, 0.0, 0.0, 0.0);
 			_speed = speed;
 		}
@@ -17,14 +22,54 @@ namespace Inteligencia {
 			_move->robot(r);
 		}
 
+		double Controller::set_x() {
+			_x = ((double)_controller->GetState().Gamepad.sThumbLX)/32768.0;
+			return _x;
+		}
+		double Controller::set_y() {
+			_y = ((double)_controller->GetState().Gamepad.sThumbLY)/32768.0;
+			return _y;
+		}
+		double Controller::set_ang() {
+			double x = ((double)_controller->GetState().Gamepad.sThumbRX)/32768.0;
+			double y = ((double)_controller->GetState().Gamepad.sThumbRY)/32768.0;
+			_ang = atan2(y, x);
+			return _ang;
+		}
+		bool Controller::should_kick() {
+			if(_controller->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER)
+			{
+				return true;
+			}
+			else return false;
+		}
+
+		void Controller::trigger_kick()
+		{
+			double strength = _controller->GetState().Gamepad.bRightTrigger/255;
+			//TODO: Botar o kick aqui				
+		}
+
+		bool Controller::should_dribble(){
+			if(_controller->GetState().Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER)
+			{
+				return true;
+			}
+			else return false;
+		}
+		void Controller::trigger_dribble(){
+			double strength = _controller->GetState().Gamepad.bLeftTrigger/255;
+			//TODO: Botar o drible aqui
+		}
 		void Controller::step() {
 			if(_controller->IsConnected()){
-				double x = ((double)_controller->GetState().Gamepad.sThumbLX)/32768.0;
-				double y = ((double)_controller->GetState().Gamepad.sThumbLY)/32768.0;
-				//cout << "x: " << x << endl;
-				//cout << "y: " << y << endl;
-				//double v = sqrt(x*x + y*y);
-				_move->set(x*_speed, y*_speed, 0.0);
+				set_x();	
+				set_y();
+				_move->set(_x*_speed, _y*_speed, 0.0);
+				if(should_kick()){
+				}
+				if(should_dribble()){
+				}
 				_move->step();
 			}
 		}
--- Inteligencia/Controller.h
+++ Inteligencia/Controller.h
@@ -8,13 +8,25 @@ namespace Inteligencia {
 
 		class Controller : public Tactic{
 		private:
-			int id;
+			int _id;
+			double _x;
+			double _y;
+			double _ang;
 			CXBOXController* _controller;
 			Robot* _robot;
 			Skills::Move * _move;
+//		Skills::Kick * _kick;
+//		Skills::Dribble * _dribble;
 			double _speed;
 				
 		public:
+			double set_x();
+			double set_y();
+			double set_ang();
+			bool should_kick();
+			void trigger_kick();
+			bool should_dribble();
+			void trigger_dribble();
 			Controller(int controller_id, Robot*, Stage*, double speed);
 			~Controller();
 			void robot(Robot*);
