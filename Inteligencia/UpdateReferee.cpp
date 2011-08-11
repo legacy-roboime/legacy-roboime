0e96310771744968334705b9359edce9c3184727
Diego Felix <diegobill@gmail.com>
2011-08-11 22:58:16 +0000

    Ideia para o Apply do referee
    git-svn-id: http://roboime.com.br/svn/RoboCup/Projeto Computacional/RobotIME/trunk@274 536dbed5-76cd-454d-90a9-4f62b6fdd354

--- Inteligencia/UpdateReferee.cpp
+++ Inteligencia/UpdateReferee.cpp
@@ -28,4 +28,24 @@ void UpdateReferee::apply(Updater* u) {
 			u->robot(k-1)->angle(_angle);
 		}
 	}*/
+
+	//BASE
+	/*char cmd_tmp;
+	rcvd_bytes_referee = e->getDados();
+	cmd_tmp = (char)rcvd_bytes_referee[0];
+	datareferee->cmd_counter = (unsigned char)rcvd_bytes_referee[1];
+	datareferee->goals_blue = (unsigned char)rcvd_bytes_referee[2];
+	datareferee->goals_yellow = (unsigned char)rcvd_bytes_referee[3];
+	//time_remaining = (int)((unsigned short)rcvd_bytes_referee[4] * (unsigned short)255 + (unsigned short)rcvd_bytes[5]);
+	datareferee->time_remaining = ((int)(rcvd_bytes_referee[4]) << 8)  + (int)(rcvd_bytes_referee[5]); //byte mais significativo
+	
+	if ( datareferee->cmd_counter != datareferee->cmd_counter_tmp ) 
+	{ // se nao houver nenhum comando novo, fazer nada
+		datareferee->cmd_counter_tmp = datareferee->cmd_counter;
+		if (datareferee->cmd != cmd_tmp)
+		{
+			datareferee->cmd_prev = datareferee->cmd;
+			datareferee->cmd = cmd_tmp;
+		}
+	}*/
 }
